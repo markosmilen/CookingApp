528f36f0493a78a24dd9b7093194691bac829dc5
marko <markosmilen@gmail.com>
2020-02-24 16:56:24 +0100

    -added progressbar to search layout -made searchbar load items -made adapter, item and rw for search

--- app/src/main/java/com/example/cookingapp/adapters/SearchAdapter.java
+++ app/src/main/java/com/example/cookingapp/adapters/SearchAdapter.java
@@ -1,19 +1,66 @@
 package com.example.cookingapp.adapters;
 
+import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-public class SearchAdapter {
+import com.bumptech.glide.Glide;
+import com.example.cookingapp.R;
+import com.example.cookingapp.models.DietMealsModel;
 
+import org.w3c.dom.Text;
+
+import java.util.ArrayList;
+
+public class SearchAdapter extends RecyclerView.Adapter<SearchAdapter.SearchViewHolder> {
+
+    ArrayList<DietMealsModel> searchedMeals;
+    LayoutInflater inflater;
+    Context context;
+
+    public SearchAdapter(ArrayList<DietMealsModel> searchedMeals, Context context) {
+        this.searchedMeals = searchedMeals;
+        this.inflater = LayoutInflater.from(context);
+        this.context = context;
+    }
+
+    @NonNull
+    @Override
+    public SearchViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = inflater.inflate(R.layout.item_search_meal, parent,false);
+        return new SearchViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull SearchViewHolder holder, int position) {
+        DietMealsModel model = searchedMeals.get(position);
+        String baseurl = "https://spoonacular.com/recipeImages/";
+        String img = baseurl + model.getImage();
+        Glide.with(context).load(img).centerCrop().into(holder.searchMealImg);
+        holder.searchMealTitle.setText(model.getTitle());
+    }
+
+    @Override
+    public int getItemCount() {
+        return searchedMeals.size();
+    }
 
     public class SearchViewHolder extends RecyclerView.ViewHolder {
 
-
+        TextView searchMealTitle;
+        ImageView searchMealImg;
 
         public SearchViewHolder(@NonNull View itemView) {
             super(itemView);
+
+            searchMealTitle = (TextView) itemView.findViewById(R.id.search_meal_name);
+            searchMealImg = (ImageView) itemView.findViewById(R.id.search_meal_image);
         }
     }
 }
--- app/src/main/java/com/example/cookingapp/fragments/TopLvlSearchFragment.java
+++ app/src/main/java/com/example/cookingapp/fragments/TopLvlSearchFragment.java
@@ -5,6 +5,7 @@ import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.text.Editable;
@@ -14,21 +15,39 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
 import android.widget.LinearLayout;
+import android.widget.ProgressBar;
 
+import com.bumptech.glide.load.model.stream.HttpUriLoader;
 import com.example.cookingapp.R;
+import com.example.cookingapp.adapters.SearchAdapter;
 import com.example.cookingapp.models.DietMealsModel;
+import com.example.cookingapp.models.DietModel;
+import com.google.gson.Gson;
 
+import org.jetbrains.annotations.NotNull;
+
+import java.io.IOException;
 import java.util.ArrayList;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.HttpUrl;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 public class TopLvlSearchFragment extends Fragment {
 
-    LinearLayout searchFields, pasta, vegetarian, asian, desserts, quick, chicken, fish, noGluten, salads ;
+    LinearLayout searchFields, results_layout, pasta, vegetarian, asian, desserts, quick, chicken, fish, noGluten, salads ;
     EditText searchBar;
     String querySelected, queryString;
     boolean isLoading, isSeachEmpty = false;
     RecyclerView recyclerView;
     ArrayList<DietMealsModel> mealsList = new ArrayList<>();
-
+    Gson gson;
+    SearchAdapter searchAdapter;
+    int offset = 0;
+    ProgressBar progressBar;
 
 
     public TopLvlSearchFragment() {
@@ -61,7 +80,15 @@ public class TopLvlSearchFragment extends Fragment {
         fish = (LinearLayout) view.findViewById(R.id.search_fish);
         noGluten = (LinearLayout) view.findViewById(R.id.search_no_gluten);
         salads = (LinearLayout) view.findViewById(R.id.search_salads);
+        results_layout = (LinearLayout) view.findViewById(R.id.results_layout);
         searchBar = (EditText) view.findViewById(R.id.search_edit_text);
+        progressBar = (ProgressBar) view.findViewById(R.id.search_progress_bar);
+        recyclerView = (RecyclerView) view.findViewById(R.id.recycler_view_results);
+
+        recyclerView.setLayoutManager(new GridLayoutManager(getContext(), 2));
+
+        gson = new Gson();
+
 
         searchBar.addTextChangedListener(new TextWatcher() {
             @Override
@@ -78,20 +105,76 @@ public class TopLvlSearchFragment extends Fragment {
             public void afterTextChanged(Editable s) {
                 queryString = s.toString();
                 if (s.toString().length() > 3) {
+
+                    searchFields.setVisibility(View.INVISIBLE);
+                    progressBar.setVisibility(View.VISIBLE);
+
+                    loadSearchedMeals(queryString, 0);
+                    searchAdapter = new SearchAdapter(mealsList, getContext());
+                    recyclerView.setAdapter(searchAdapter);
+                    searchAdapter.notifyDataSetChanged();
+
+
                     //            loadMeals(queryString);
                 } else {
                     mealsList = new ArrayList<>();
-                    //       searchAdapter = new SearchAdapter(mealsList, TopLvlSearchFragment.this);
+                    searchFields.setVisibility(View.VISIBLE);
+                    results_layout.setVisibility(View.INVISIBLE);
+                    progressBar.setVisibility(View.INVISIBLE);
+                }
+            }
+        });
+        return view;
+    }
 
+    public void loadSearchedMeals(String query, int offset){
+        mealsList.clear();
+        isLoading = true;
+        HttpUrl.Builder builder = HttpUrl.parse("https://api.spoonacular.com/recipes/search").newBuilder();
+        builder.addQueryParameter("query", query);
+        builder.addQueryParameter("number", "30");
+        if (offset != 0){
+            builder.addQueryParameter("offset", offset +"");
+        }
+        builder.addQueryParameter("apiKey", "538bac8dcbdc467c9c1683802b57809b");
+        String url = builder.build().toString();
+
+        OkHttpClient client = new OkHttpClient();
+
+        final Request request = new Request.Builder().url(url).build();
+
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(@NotNull Call call, @NotNull IOException e) {
+
+            }
+
+            @Override
+            public void onResponse(@NotNull Call call, @NotNull Response response) throws IOException {
+                if(response.isSuccessful()){
+                    String jsonstring = response.body().string();
+                    DietModel model = gson.fromJson(jsonstring, DietModel.class);
+                    ArrayList<DietMealsModel> result = model.getResults();
+                    for (int i = 0; i < result.size(); i++){
+                        mealsList.add(result.get(i));
+                    }
+
+                    if (getActivity() != null){
+                        getActivity().runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                searchAdapter.notifyDataSetChanged();
+                                isLoading = false;
+                                progressBar.setVisibility(View.GONE);
+                                results_layout.setVisibility(View.VISIBLE);
+                            }
+                        });
+                    }
                 }
             }
         });
 
-        return view;
     }
-
-
-
 
     public void onSectionClicked(View view){
         switch (view.getId()){
